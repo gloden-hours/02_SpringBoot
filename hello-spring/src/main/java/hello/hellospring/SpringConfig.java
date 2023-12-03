de30505f5f8dd3d0b108932c494de33ce2c04ba8
DESKTOP-L81L9DC\blind <gold46548@gmail.com>
2023-12-03 16:07:30 +0900

    23.12.02 공부(Jdbc, JPA)

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,27 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    //private final DataSource dataSource;
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.em = em;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,7 +30,10 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
+
+
 
 }
--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -2,10 +2,13 @@ package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Transactional
 public class MemberService {    //테스트 단축키 : ctrl + shift + T
     private final MemberRepository memberRepository;
 
@@ -16,13 +19,13 @@ public class MemberService {    //테스트 단축키 : ctrl + shift + T
     //회원가입
     public Long join(Member member) {
         //같은 이름이 있는 중복된 회원X
-        validaeDuplicateMember(member); //리팩토링 메뉴열기 : Ctrl + Alt + Shift + T
+        validateDuplicateMember(member); //리팩토링 메뉴열기 : Ctrl + Alt + Shift + T
                                         //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
     }
 
-    private void validaeDuplicateMember(Member member) {
+    private void validateDuplicateMember(@NotNull Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
